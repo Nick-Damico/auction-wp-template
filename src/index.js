b099650dfeb4365d254a8fafc093c1e3167edc39
Nick-Damico <nickalan82@icloud.com>
2018-06-30 19:47:33 -0400

    moved import of package .css files to entry point index.js from sass/main.scss, issue with styles not being included when running build:prod script

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 // Import styles
 import './sass/main.scss';
+
+// Import Styles from node_modules
+import "flickity/dist/flickity.css";
+import "lightbox2/dist/css/lightbox.css";
+
 // Import javaScript Modules
 import Navigation from './modules/Navigation';
 import Flickity from 'flickity';
